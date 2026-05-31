ed583f84225ab894ba85bf7d816fea18131f40ac
draxc <sasacikota97@gmail.com>
2026-05-31 13:46:37 +0300

    +fix

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d3_0gc14tf7n9_#q6evg-9jwojoq+s$jz4*i3hsjco30g&0l8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -116,4 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
