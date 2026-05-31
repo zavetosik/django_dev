05fcd51b303580062f2a9fdee7366e694da40720
draxc <sasacikota97@gmail.com>
2026-05-31 13:19:10 +0300

    +finish

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import TaskForm
 from .models import Task
 
 
@@ -10,5 +12,19 @@ def about(request):
     return render(request, 'main/about.html')
 
 def create(request):
-    return render(request, 'main/create.html')
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма неверная'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error,
+    }
+    return render(request, 'main/create.html', context)
 
